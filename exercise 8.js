c58e8706b4133ed7e0eb5ed0d06d389bb9496600
Amarev0001 <92624503+Amarev0001@users.noreply.github.com>
2021-11-01 20:16:55 +0300

    Update exercise 8.js

--- exercise 8.js
+++ exercise 8.js
@@ -6,9 +6,9 @@ days.set(4, "Thursday");
 days.set(5, "Friday");
 days.set(6, "Saturday");
 days.set(7, "Sunday"); 
-for (let name of days.keys()) { //Ïîëó÷àåì êëþ÷è ìàññèâà
+for (let name of days.keys()) {
 	console.log(name);
 }
-days.forEach((value, key) => { //Âûâîä â êîíñîëü çíà÷åíèé â çàäàííîì âèäå
+days.forEach((value, key) => {
 	console.log(`Дунь - ${key}, Месяц - ${value}`);
 });
